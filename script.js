f7ff20a6de20e9ebea120c597739939007b404b6
ashish <ashishmasih92g@gmail.com>
2025-04-03 23:32:55 +0530

    assignment

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+
+let strong = document.querySelectorAll("strong")
+
+console.log(strong)
+
 function highlight() {
     //Write your code here
-
+    for (let t of strong) {
+        t.style.color = "#008002"
+    }
 
 }
 
 
 function return_normal() {
     //Write your code here
+for(let t of strong){
+    t.style.color= "	#000000"
+}
 
-    
 }
